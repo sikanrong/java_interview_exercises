a2648a3757cbd327f00b83301c18b607d6e289c0
Alex Pilafian <sikanrong@Alexs-MacBook-Pro.local>
2018-09-08 09:31:11 +0100

    Finally making my recursive matrix multiplication work for matrices of odd-dimensionality (which make lots of complications arise during the partition step)

--- src/com/sikanrong/practice/exercises/concurrency/MatrixMultiplyParallel.java
+++ src/com/sikanrong/practice/exercises/concurrency/MatrixMultiplyParallel.java
@@ -32,10 +32,19 @@ public class MatrixMultiplyParallel {
 				this.dims[0] = dimsx;
 				this.dims[1] = dimsy;
 			}
+			
+			public boolean hasZeroDims() {
+				return Math.min(dims[0], dims[1]) == 0;
+			}
+
+			public boolean isSingleElement() {
+				return (int) Math.max(dims[0], dims[1]) == 1;
+			}
 		}
 
 		private Object data[][];
 		private int[] dims = new int[2];
+		private Object defaultValue;
 
 		public Matrix(T[][] _d) {
 			data = _d;
@@ -57,45 +66,70 @@ public class MatrixMultiplyParallel {
 
 		@SuppressWarnings("unchecked")
 		public T get(int x, int y) {
-			return (T) data[y][x];
+			Object toGet = data[y][x];
+			return (T) ((toGet == null)? this.defaultValue : toGet);
 		}
 
 		public void set(T t, int x, int y) {
 			data[y][x] = t;
+		}
+		
+		public void setDefault(T t) {
+			this.defaultValue = t;
 		}
 
 		public int[] getDims() {
 			return dims;
 		}
 
-		public Partition partition(int x, int y) {
-			return this.partition(x, y, this.fullPartition());
+		public Partition fpart(int x, int y, Partition p) {
+			return this.partition(x, y, p, true);
 		}
-
-		public Partition partition(int x, int y, Partition p) {
+		
+		public Partition cpart(int x, int y, Partition p) {
+			return this.partition(x, y, p, false);
+		}
+		
+		public Partition partition(int x, int y, Partition p, boolean floorwise) {
 			// floorwise partition is equal to ceilwise partition WLOG for all
 			// matrices of odd dimensionality.
 
-			int hf = (int) Math.floor(p.dims[0] / 2);
-			int hc = (int) Math.ceil(p.dims[0] / 2);
-			int vf = (int) Math.floor(p.dims[0] / 2);
-			int vc = (int) Math.ceil(p.dims[0] / 2);
+			int hf = (int) Math.floor((float)p.dims[0] / 2f);
+			int hc = (int) Math.ceil((float)p.dims[0] / 2f);
+			int vf = (int) Math.floor((float)p.dims[1] / 2f);
+			int vc = (int) Math.ceil((float)p.dims[1] / 2f);
 
-			int viewX = p.view[0] + (x * hf);
-			int viewY = p.view[1] + (y * vf);
-			int dimsX = (x == 0) ? hf : hc;
-			int dimsY = (y == 0) ? vf : vc;
-
+			int viewX;
+			int viewY;
+			int dimsX;
+			int dimsY;
+			
+			if(floorwise) {
+				viewX = p.view[0] + (x * hf);
+				viewY = p.view[1] + (y * vf);
+				dimsX = (x == 0)? hf : hc;
+				dimsY = (y == 0)? vf : vc;
+			}else {
+				viewX = p.view[0] + (x * hc);
+				viewY = p.view[1] + (y * vc);
+				dimsX = (x == 0)? hc : hf;
+				dimsY = (y == 0)? vc : vf;
+			}
+			
+			//Avoids double-counting when we end up with a [1x1] * [2x1]
+			//Type of situation.
+			if(p.dims[0] == 1 && p.dims[1] == 1) {
+				dimsX = 1;
+				dimsY = 1;
+			}
+			
 			return new Partition(viewX, viewY, dimsX, dimsY);
 		}
 
 		public Partition fullPartition() {
 			return new Partition(0, 0, dims[0], dims[1]);
 		}
-
-		public static boolean isSingleElement(Partition p) {
-			return (int) Math.max(p.dims[0], p.dims[1]) == 1;
-		}
+		
 		
 		@SuppressWarnings("unchecked")
 		public T[][] toArray(){
@@ -104,10 +138,23 @@ public class MatrixMultiplyParallel {
 
 	}
 
-	private static void matrixMultiplyRecursive(Matrix<Integer> c, Matrix<Integer> a, Matrix<Integer> b,
-
-			Matrix.Partition pc, Matrix.Partition pa, Matrix.Partition pb) {
-		if (Matrix.isSingleElement(pc) && Matrix.isSingleElement(pa) && Matrix.isSingleElement(pb)) {
+	private static void matrixMultiplyRecursive(
+			Matrix<Integer> c, 
+			Matrix<Integer> a, 
+			Matrix<Integer> b,
+			Matrix.Partition pc, 
+			Matrix.Partition pa, 
+			Matrix.Partition pb
+	) {
+		if( pc.hasZeroDims() || 
+			pa.hasZeroDims() || 
+			pb.hasZeroDims() ) {
+			return;
+		} else if (
+			pc.isSingleElement() && 
+			pa.isSingleElement() && 
+			pb.isSingleElement()
+		) {
 			int _a = a.get(pa.view[0], pa.view[1]);
 			int _b = b.get(pb.view[0], pb.view[1]);
 			int p = _a * _b;
@@ -115,31 +162,36 @@ public class MatrixMultiplyParallel {
 		} else {
 			Matrix<Integer> t = new Matrix<>(c.getDims());
 
-			matrixMultiplyRecursive(c, a, b, c.partition(0, 0, pc), a.partition(0, 0, pa), b.partition(0, 0, pb));
-			matrixMultiplyRecursive(t, a, b, t.partition(0, 0, pc), a.partition(1, 0, pa), b.partition(0, 1, pb));
+			matrixMultiplyRecursive(c, a, b, c.fpart(0, 0, pc), a.fpart(0, 0, pa), b.fpart(0, 0, pb));
+			matrixMultiplyRecursive(t, a, b, t.fpart(0, 0, pc), a.fpart(1, 0, pa), b.fpart(0, 1, pb));
 			
-			matrixMultiplyRecursive(c, a, b, c.partition(1, 0, pc), a.partition(0, 0, pa), b.partition(1, 0, pb));
-			matrixMultiplyRecursive(t, a, b, t.partition(1, 0, pc), a.partition(1, 0, pa), b.partition(1, 1, pb));
+			matrixMultiplyRecursive(c, a, b, c.fpart(1, 0, pc), a.fpart(0, 0, pa), b.fpart(1, 0, pb));
+			matrixMultiplyRecursive(t, a, b, t.fpart(1, 0, pc), a.fpart(1, 0, pa), b.fpart(1, 1, pb));
 			
-			matrixMultiplyRecursive(c, a, b, c.partition(0, 1, pc), a.partition(0, 1, pa), b.partition(0, 0, pb));
-			matrixMultiplyRecursive(t, a, b, t.partition(0, 1, pc), a.partition(1, 1, pa), b.partition(0, 1, pb));
+			matrixMultiplyRecursive(c, a, b, c.fpart(0, 1, pc), a.fpart(0, 1, pa), b.fpart(0, 0, pb));
+			matrixMultiplyRecursive(t, a, b, t.fpart(0, 1, pc), a.fpart(1, 1, pa), b.fpart(0, 1, pb));
 			
-			matrixMultiplyRecursive(c, a, b, c.partition(1, 1, pc), a.partition(0, 1, pa), b.partition(1, 0, pb));
-			matrixMultiplyRecursive(t, a, b, t.partition(1, 1, pc), a.partition(1, 1, pa), b.partition(1, 1, pb));
+			matrixMultiplyRecursive(c, a, b, c.fpart(1, 1, pc), a.fpart(0, 1, pa), b.fpart(1, 0, pb));
+			matrixMultiplyRecursive(t, a, b, t.fpart(1, 1, pc), a.fpart(1, 1, pa), b.fpart(1, 1, pb));
 
-			IntStream.range(pc.view[0], pc.view[0] + pc.dims[0]).forEach(i -> {
-				IntStream.range(pc.view[1], pc.view[1] + pc.dims[1]).forEach(j -> {
+			for(int i = pc.view[0]; i < (pc.view[0] + pc.dims[0]); i++) {
+				for(int j = pc.view[1]; j < (pc.view[1] + pc.dims[1]); j++) {
 					int _c = c.get(i, j);
 					int _t = t.get(i, j);
 					int s = _c + _t;
 					c.set(s, i, j);
-				});
-			});
+				};
+			};
 		}
 	}
 
 	public static Matrix<Integer> multiply(Matrix<Integer> a, Matrix<Integer> b) {
 		Matrix<Integer> c = new Matrix<>(a.getDims());
+		
+		a.setDefault(0);
+		b.setDefault(0);
+		c.setDefault(0);
+		
 		matrixMultiplyRecursive(c, a, b, c.fullPartition(), a.fullPartition(), b.fullPartition());
 		return c;
 	}
--- test/com/sikanrong/practice/exercises/test/concurrency/MatrixMultiplyParallelTest.java
+++ test/com/sikanrong/practice/exercises/test/concurrency/MatrixMultiplyParallelTest.java
@@ -2,14 +2,17 @@ package com.sikanrong.practice.exercises.test.concurrency;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 
 import com.sikanrong.practice.exercises.concurrency.MatrixMultiplyParallel;
 import com.sikanrong.practice.exercises.concurrency.MatrixMultiplyParallel.Matrix;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MatrixMultiplyParallelTest {
-	@SuppressWarnings("unused")
+	
 	@Test 
-	void testMatrixMultiply() {
+	void testMatrixMultiplyEven() {
 		Integer[][] ar_a = {
 			{1,2},
 			{3,4}
@@ -30,7 +33,71 @@ public class MatrixMultiplyParallelTest {
 		Matrix<Integer> actual = MatrixMultiplyParallel.multiply(a, b);
 		Matrix<Integer> expected = new Matrix<>(ar_e);
 		
-		assertArrayEquals(actual.toArray(), expected.toArray());
+		assertArrayEquals(expected.toArray(), actual.toArray());
 		
 	}
+	
+	@Test 
+	void testMatrixMultiplyOdd() {
+		Integer[][] ar_a = {
+			{1,2,3},
+			{4,5,6},
+			{7,8,9}
+		};
+		
+		Integer[][] ar_b = {
+			{1,2,3},
+			{4,5,6},
+			{7,8,9}
+		};
+		
+		Integer[][] ar_e = {
+			{30, 36, 42}, 
+			{66, 81, 96}, 
+			{102, 126, 150}
+		};
+		
+		Matrix<Integer> a = new Matrix<>(ar_a);
+		Matrix<Integer> b = new Matrix<>(ar_b);
+		Matrix<Integer> actual = MatrixMultiplyParallel.multiply(a, b);
+		Matrix<Integer> expected = new Matrix<>(ar_e);
+		
+		assertArrayEquals(expected.toArray(), actual.toArray());
+		
+	}
+	
+	
+	@Test 
+	void testMatrixMultiplyEven2() {
+		Integer[][] ar_a = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9,10,11,12}, {13,14,15,16}};
+		Integer[][] ar_b = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9,10,11,12}, {13,14,15,16}};
+		
+		Integer[][] ar_e = {{90, 100, 110, 120}, {202, 228, 254, 280}, {314, 356, 398, 440}, {426, 484, 542, 600}};
+		
+		Matrix<Integer> a = new Matrix<>(ar_a);
+		Matrix<Integer> b = new Matrix<>(ar_b);
+		Matrix<Integer> actual = MatrixMultiplyParallel.multiply(a, b);
+		Matrix<Integer> expected = new Matrix<>(ar_e);
+		
+		assertArrayEquals(expected.toArray(), actual.toArray());
+		
+	}
+	
+	@Test 
+	void testMatrixMultiplyNonSquare() {
+		Integer[][] ar_a = {{1, 2, 3}, {5, 6, 7}, {9,10,11}, {13,14,15}};
+		Integer[][] ar_b = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9,10,11,12}};
+		
+		Integer[][] ar_e = {{38, 44, 50, 56}, {98, 116, 134, 152}, {158, 188, 218, 248}, {218, 260, 302, 344}};
+		
+		Matrix<Integer> a = new Matrix<>(ar_a);
+		Matrix<Integer> b = new Matrix<>(ar_b);
+		Matrix<Integer> actual = MatrixMultiplyParallel.multiply(a, b);
+		Matrix<Integer> expected = new Matrix<>(ar_e);
+		
+		assertArrayEquals(expected.toArray(), actual.toArray());
+		
+	}
+	
+	
 }
